1264462bbea359610a3f9756e14d7d222332fa5c
hello <pooh@poohprod.ru>
2022-09-30 20:36:27 +0300

    add meta info for pages

--- read_topic.php
+++ read_topic.php
@@ -17,6 +17,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Закрытый форум Харибда, <?=htmlspecialchars($stdout->FirstPost($topic['id']))?>">
 	<title><?=$topic['name']?></title>
 	<link rel="stylesheet" href="css/navbar.css">
 </head>
