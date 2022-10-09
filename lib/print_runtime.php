76fb160a401261a9772156abc96a1765b2e3afb7
hello <pooh@poohprod.ru>
2022-10-09 18:14:25 +0300

    fix print runtime func

--- lib/print_runtime.php
+++ lib/print_runtime.php
@@ -9,5 +9,5 @@
 
 	$endTime = new DateTime('now');
 	$runTime = $startTime->diff($endTime);
-	if (!empty($_SESSION['USER']['username']) && !strcmp($_SESSION['USER']['username'], 'pooh'))
+	if (!empty($_SESSION['USER']['login']) && !strcmp($_SESSION['USER']['login'], 'pooh'))
 		echo $runTime->format('<span style="background:red;color:white;position:fixed;top:0;right:0;opacity:0.4;">%S секунд, %f микросекунд</span>');
