b1caadb3af351b83e3a9c272c3ddbbcd060a684b
hello <pooh@poohprod.ru>
2022-10-17 21:20:43 +0300

    fix desktop interface, error auth page (forum.php)

--- forum.php
+++ forum.php
@@ -15,15 +15,18 @@
 
 	<main class="container-fluid pb-5">
 
+<?php
+
+	#
+	# Закрываем страницу для не авторизованных пользователей
+	require 'lib/auth.class.php';
+	#
+	# Системный класс
+	require 'lib/system.class.php';
+?>
+
 		<aside class="d-none d-lg-block m-5 float-end" style="height:1000px;">
 			<ul class="sticky-top list-group shadow-sm">
-				<!-- <a class="list-group-item list-group-item-action px-5" href="forum.php">
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="darkred" class="bi bi-chat-square-text" viewBox="0 0 16 16">
-					  <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-					  <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
-					</svg>
-					Темы
-				</a> -->
 				<a class="list-group-item list-group-item-action px-5 bg-success bg-gradient text-white" href="create_topic.php">
 					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-bookmark-plus" viewBox="0 0 16 16">
 						  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
@@ -41,14 +44,8 @@
 			</ul>
 		</aside>
 
-<?php
 
-	#
-	# Закрываем страницу для не авторизованных пользователей
-	require 'lib/auth.class.php';
-	#
-	# Системный класс
-	require 'lib/system.class.php';
+<?php
 
 	$topics = $stdout->ListTopics();
 	$curr_category = 0;
