ba53f6a7ee961181119514b8f8f11dfc62a4fba5
hello <pooh@poohprod.ru>
2022-11-28 21:17:20 +0300

    fix edit button for profile

--- profile.php
+++ profile.php
@@ -41,13 +41,13 @@
 
 			<aside class="sticky-top d-none d-lg-block m-5 float-end" style="height: 100%; max-width: 25%;">
 					<div class="list-group shadow-sm">
-						<a class="list-group-item list-group-item-action px-5 bg-danger text-light" href="#" onclick="history.go(-1); event.preventDefault();">
+						<a class="list-group-item list-group-item-action px-5 bg-danger bg-gradient text-light" href="#" onclick="history.go(-1); event.preventDefault();">
 							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
 							  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
 							</svg>
 							Закрыть
 						</a>
-						<?php if (!strcmp($_SESSION['USER']['username'], $_GET['user'])) { ?>
+						<?php if (!strcmp($_SESSION['USER']['username'], htmlspecialchars($_GET['user']))) { ?>
 							<a class="list-group-item list-group-item-action px-5 bg-light" href="settings.php">
 								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
 								  <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
